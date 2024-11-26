81f64263482e1fbbb10729ad4dabd63d73d386a0
Morgan Gravelat <mgravelat@gmail.com>
2024-11-26 18:46:39 -0500

    Update gen_code.h

--- gen_code.h
+++ gen_code.h
@@ -3,11 +3,14 @@
 
 #include "ast.h"
 
+// enum {
+//     gp,sp,fp,r3,r4,r5,r6,ra;
+// }
 // Initialize the code generation module.
-void gen_code_initialize(const char *output_filename);
+void gen_code_initialize();
 
 // Generate VM code for the given program AST.
 // This function is called after parsing and AST creation is complete.
-void gen_code_program(block_t program);
+void gen_code_program(BOFFILE bf, block_t program);
 
 #endif
