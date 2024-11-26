8b03b6d7586ad76a4a78cd312e1cd25db94eda6d
Morgan Gravelat <mgravelat@gmail.com>
2024-11-26 18:46:11 -0500

    Update literal_table.h

--- literal_table.h
+++ literal_table.h
@@ -1,32 +1,17 @@
 #ifndef _LITERAL_TABLE_H
 #define _LITERAL_TABLE_H
 
-#include <stdbool.h>
+#include "bof.h"
 
+// Define `float_type` as float for consistency
 typedef float float_type;
 
-// Initialize the literal table
 void literal_table_initialize();
-
-// Find the offset of a literal in the table; return -1 if not found
-int literal_table_find_offset(const char *sought, float_type value);
-
-// Return the size of the literal table
+unsigned int literal_table_add(float_type value);
 unsigned int literal_table_size();
-
-// Check if a literal is present in the table
-bool literal_table_present(const char *sought, float_type value);
-
-// Lookup or add a literal to the table and return its offset
-unsigned int literal_table_lookup(const char *text, float_type value);
-
-// Start an iteration over the literal table
+void literal_table_finalize(BOFFILE bf);
 void literal_table_start_iteration();
-
-// End the current iteration over the literal table
 void literal_table_end_iteration();
-
-// Get the next literal value during iteration
 float_type literal_table_iteration_next();
 
 #endif
